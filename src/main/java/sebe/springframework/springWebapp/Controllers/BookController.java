beffeeab85416f03033297d473db7e3dfae3487b
Seppe berghmans <seppeberghmans@gmail.com>
2019-02-18 19:53:12 +0100

    initial push

---
+++ src/main/java/sebe/springframework/springWebapp/Controllers/BookController.java
@@ -0,0 +1,25 @@
+package sebe.springframework.springWebapp.Controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sebe.springframework.springWebapp.repositories.BookRepository;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books",bookRepository.findAll());
+
+        return "books";
+    }
+}
